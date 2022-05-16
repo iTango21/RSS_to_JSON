013f290f65f0bad9abf5e58ec98e1a3eb0ff1ee8
iTango21 <itango21@gmail.com>
2022-05-16 12:26:00 +0300

    links from external file...

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ import feedparser
 import json
 
 
-feed = feedparser.parse('http://feeds.bbci.co.uk/news/rss.xml#')
+#feed = feedparser.parse('http://feeds.bbci.co.uk/news/rss.xml#')
 
-urls = ['http://feeds.bbci.co.uk/news/rss.xml#',
-        'http://feeds.nature.com/nmat/rss/current',
-        'https://rss.nytimes.com/services/xml/rss/nyt/Sports.xml',
-        'http://feeds.bbci.co.uk/news/rss.xml#',
-        'https://news.google.com/rss/search?q=weird+AND+OR+hundred+OR+thousand+OR+million+OR+billion&num=100&newwindow=1&safe=off&client=safari&rls=en&biw=1415&bih=1086&um=1&ie=UTF-8&hl=en-US&gl=US&ceid=US:en',
-        'http://feeds.bbci.co.uk/news/world/africa/rss.xml',
-        'https://news.google.com/rss/search?q=Elon%20Musk&ceid=US:en&hl=en-US&gl=US',
+# urls = ['http://feeds.bbci.co.uk/news/rss.xml#',
+#         'http://feeds.nature.com/nmat/rss/current',
+#         'https://rss.nytimes.com/services/xml/rss/nyt/Sports.xml',
+#         'http://feeds.bbci.co.uk/news/rss.xml#',
+#         'https://news.google.com/rss/search?q=weird+AND+OR+hundred+OR+thousand+OR+million+OR+billion&num=100&newwindow=1&safe=off&client=safari&rls=en&biw=1415&bih=1086&um=1&ie=UTF-8&hl=en-US&gl=US&ceid=US:en',
+#         'http://feeds.bbci.co.uk/news/world/africa/rss.xml',
+#         'https://news.google.com/rss/search?q=Elon%20Musk&ceid=US:en&hl=en-US&gl=US',
+#
+#     ]
 
-    ]
+# читаю ССЫЛКИ из ранее созданного файла
+# !!! ОБРЕЗАЮ СИМВОЛ ПЕРЕНОСА СТРОКИ !!!
+with open('in/links.txt') as file:
+        urls = [line.strip() for line in file.readlines()]
 
 feeds = [feedparser.parse(url)['entries'] for url in urls]
 #print(feeds[1][0].keys())
